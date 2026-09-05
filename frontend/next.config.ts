f1cde0a7de36e1c521de21fbad02adcea0ca837c
Waleed-Ahmad0 <waleedahmad1117@gmail.com>
2026-09-05 12:41:44 +0500

    rewrite nextconfig file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,24 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    const backendUrl = process.env.BACKEND_URL || "http://localhost:4000";
+    return [
+      {
+        source: "/auth/:path*",
+        destination: `${backendUrl}/auth/:path*`,
+      },
+      {
+        source: "/webhook/:path*",
+        destination: `${backendUrl}/webhook/:path*`,
+      },
+      {
+        source: "/api/:path*",
+        destination: `${backendUrl}/api/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
